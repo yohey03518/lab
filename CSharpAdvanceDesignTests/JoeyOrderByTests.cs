999a500c75533a488b08c043744dbcc29a8b77e2
Joey Chen <jay.hatelove@gmail.com>
2019-10-06 13:58:32 +0800

    move class to file

--- CSharpAdvanceDesignTests/JoeyOrderByTests.cs
+++ CSharpAdvanceDesignTests/JoeyOrderByTests.cs
@@ -3,39 +3,9 @@ using ExpectedObjects;
 using Lab.Entities;
 using NUnit.Framework;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace CSharpAdvanceDesignTests
 {
-    public static class MyLinqExtensions
-    {
-        public static IEnumerable<Employee> JoeyOrderByComboComparer(this IEnumerable<Employee> employees,
-            IComparer<Employee> comparer)
-        {
-            //bubble sort
-            var elements = employees.ToList();
-            while (elements.Any())
-            {
-                var minElement = elements[0];
-                var index = 0;
-                for (int i = 1; i < elements.Count; i++)
-                {
-                    var employee = elements[i];
-                    var finalCompareResult = comparer.Compare(employee, minElement);
-
-                    if (finalCompareResult < 0)
-                    {
-                        minElement = employee;
-                        index = i;
-                    }
-                }
-
-                elements.RemoveAt(index);
-                yield return minElement;
-            }
-        }
-    }
-
     [TestFixture]
     public class JoeyOrderByTests
     {
